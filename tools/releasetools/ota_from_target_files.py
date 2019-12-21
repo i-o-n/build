2c600dbd6eb6d313f7bfc451a6ebe98e5cc83b5f
AnkitGourav <Ankitgourav035@gmail.com>
2019-12-21 10:11:10 +0530

    Revert "build: Ensure /system unmounted at install start"
    This reverts commit 359a8027a53f58576dbad2d76b047d7f2af21542.
    
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -951,8 +951,6 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   # Dump fingerprints
   script.Print("Target: {}".format(target_info.fingerprint))
 
-  script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
-
   android_version = target_info.GetBuildProp("ro.build.version.release")
   ionizer_version = target_info.GetBuildProp("ro.ionizer")
   build_id = target_info.GetBuildProp("ro.build.id")
